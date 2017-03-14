43f200958b6887f6e8f6515361055c01d6718971
Jamile Villarroel Yabar <Jamile Villarroel Yabar>
2017-03-14 18:40:45 -0500

    hola

---
+++ BankingSystem/AccountTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankingModel;
+
+namespace AccountTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account cuentaJ = new Account();
+            cuentaJ.Nombre = "Jamile";
+            cuentaJ.Edad = 20;
+            cuentaJ.Numero = 984773868;
+            cuentaJ.Correo = "jamilevillarroel7@gmail.com";
+
+            Console.WriteLine("El nombre de la persona es:{0}" +
+                              "Su edad es: {1} " +
+                              "Su numero de celular es: {2} " +
+                              "Su correo es: {3}", cuentaJ.Nombre,cuentaJ.Edad,cuentaJ.Numero,cuentaJ.Correo);
+
+
+
+            
+
+        }
+    }
+}
---
+++ BankingSystem/BankingModel/Account.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingModel
+{
+    public class Account
+    {
+        private string _name;
+        private int _edad;
+        private int _numero;
+        private string _correo;
+
+        public string Nombre
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public int Edad
+        {
+            get
+            {
+                return _edad;
+            }
+
+            set
+            {
+                _edad = value;
+            }
+        }
+
+        public int Numero
+        {
+            get
+            {
+                return _numero;
+            }
+
+            set
+            {
+                _numero = value;
+            }
+        }
+
+        public string Correo
+        {
+            get
+            {
+                return _correo;
+            }
+
+            set
+            {
+                _correo = value;
+            }
+        }
+
+        public Account()
+        {
+
+        }
+
+        public Account(string name,int edad,int numero, string correo)
+        {
+            _name = name;
+            _edad = edad;
+            _numero = numero;
+            _correo = correo;
+        }
+    }
+
+}
